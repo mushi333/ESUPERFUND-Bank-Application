86c5dfb8c0b115d9008617d9a1876e58d260b7c0
mushi333 <mujtaba.zahidi24@gmail.com>
2023-11-04 13:36:06 +1100

    Updated logging

--- HTTP-API/Controllers/TransactionController.cs
+++ HTTP-API/Controllers/TransactionController.cs
@@ -21,6 +21,16 @@ public class TransactionController : ControllerBase
         _dbContext = dbContext;
     }
 
+    // [HttpGet("api/CalculateClosingBalance")]
+    // public async Task<ActionResult> CalculateClosingBalance()
+    // {
+    //     var rawBankTransactions = await _dbContext.BankTransactions.ToListAsync();
+
+    //     var rawBankBalancesClosed = "";
+    //     _logger.LogInformation("Closed the following bank balances: " + rawBankBalancesClosed);
+    //     return Ok();
+    // }
+
     [HttpGet("api/BankTransaction/all")]
     public async Task<ActionResult> GetBankTransactionAsync()
     {
@@ -33,6 +43,13 @@ public class TransactionController : ControllerBase
     public async Task<ActionResult> GetSingleBankTransactionAsync([FromQuery] Guid id)
     {
         var bankTransaction = await _dbContext.BankTransactions.FindAsync(id);
+        if (bankTransaction == null)
+        {
+            _logger.LogInformation(
+                "Could not retrieve the bank transaction: id = " + id.ToString()
+            );
+            return NotFound("Id: " + id + " does not exist");
+        }
         _logger.LogInformation("Retrieved the bank transaction: id = " + id.ToString());
         return Ok(bankTransaction);
     }
@@ -49,6 +66,13 @@ public class TransactionController : ControllerBase
     public async Task<ActionResult> GetSingleRawBankTransactionAsync([FromQuery] Guid id)
     {
         var rawBankTransaction = await _dbContext.RawBankTransactions.FindAsync(id);
+        if (rawBankTransaction == null)
+        {
+            _logger.LogInformation(
+                "Could not retrieve the raw bank transaction: id = " + id.ToString()
+            );
+            return NotFound("Id: " + id + " does not exist");
+        }
         _logger.LogInformation("Retrieved raw bank transaction: id = " + id.ToString());
         return Ok(rawBankTransaction);
     }
@@ -71,29 +95,51 @@ public class TransactionController : ControllerBase
         [FromBody] RawBankTransaction rawBankTransaction
     )
     {
-        _dbContext.RawBankTransactions.Any(r => r.Id == rawBankTransaction.Id);
-        _dbContext.Entry(rawBankTransaction).State = EntityState.Modified;
-        await _dbContext.SaveChangesAsync();
-        _logger.LogInformation(
-            "Updated the raw bank transaction: id = " + rawBankTransaction.Id.ToString()
-        );
-        return NoContent();
+        try
+        {
+            _dbContext.RawBankTransactions.Any(r => r.Id == rawBankTransaction.Id);
+            _dbContext.Entry(rawBankTransaction).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            _logger.LogInformation(
+                "Updated the raw bank transaction: id = " + rawBankTransaction.Id.ToString()
+            );
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            var errorMessage =
+                "Could not update the raw bank transaction: id = "
+                + rawBankTransaction.Id.ToString();
+            _logger.LogInformation(errorMessage);
+            _logger.LogError(ex.Message);
+            return Conflict(errorMessage);
+        }
     }
 
     [HttpDelete("api/RawBankTransaction")]
     public async Task<IActionResult> DeleteRawBankTransactionAsync([FromQuery] Guid id)
     {
-        var rawBankTransaction = await _dbContext.RawBankTransactions.FindAsync(id);
-
-        if (rawBankTransaction == null)
+        try
         {
-            _logger.LogInformation("Could not find the raw bank transaction: id = " + id);
-            return NotFound();
+            var rawBankTransaction = await _dbContext.RawBankTransactions.FindAsync(id);
+            if (rawBankTransaction == null)
+            {
+                _logger.LogInformation("Could not find the raw bank transaction: id = " + id);
+                return NotFound();
+            }
+            _dbContext.RawBankTransactions.Remove(rawBankTransaction);
+            await _dbContext.SaveChangesAsync();
+            _logger.LogInformation("Deleted the raw bank transaction: id = " + id);
+            return NoContent();
         }
-
-        _dbContext.RawBankTransactions.Remove(rawBankTransaction);
-        await _dbContext.SaveChangesAsync();
-        _logger.LogInformation("Deleted the raw bank transaction: id = " + id);
-        return NoContent();
+        catch (Exception ex)
+        {
+            var errorMessage =
+                "Could not delete the raw bank transaction: id = "
+                + id.ToString();
+            _logger.LogInformation(errorMessage);
+            _logger.LogError(ex.Message);
+            return Conflict(errorMessage);
+        }
     }
 }
