c9523975f24d34d8949862fca4b5f0698c122c59
mushi333 <mujtaba.zahidi24@gmail.com>
2023-11-04 14:33:51 +1100

    Added in closing of the raw bank balances

--- HTTP-API/Controllers/TransactionController.cs
+++ HTTP-API/Controllers/TransactionController.cs
@@ -21,20 +21,87 @@ public class TransactionController : ControllerBase
         _dbContext = dbContext;
     }
 
-    // [HttpGet("api/CalculateClosingBalance")]
-    // public async Task<ActionResult> CalculateClosingBalance()
-    // {
-    //     var rawBankTransactions = await _dbContext.BankTransactions.ToListAsync();
+    [HttpGet("api/CalculateClosingBalance")]
+    public async Task<ActionResult> CalculateClosingBalance()
+    {
+        try
+        {
+            var rawBankTransactions = await _dbContext.RawBankTransactions.ToListAsync();
+            var rawBankTransactionToCarryOver = new List<RawBankTransaction>();
+            var rawBankTransactionAccountNumbers = await _dbContext.RawBankTransactions
+                .Where(r => r.AccountNumber != null)
+                .Select(r => r.AccountNumber)
+                .Distinct()
+                .ToListAsync();
+            var rawBankBalancesClosed = new List<string>();
 
-    //     var rawBankBalancesClosed = "";
-    //     _logger.LogInformation("Closed the following bank balances: " + rawBankBalancesClosed);
-    //     return Ok();
-    // }
+            foreach (int? accountNumber in rawBankTransactionAccountNumbers)
+            {
+                bool valid = true;
+                int previousBalance = 0;
+                var accountEntries = await _dbContext.RawBankTransactions
+                    .Where(r => r.AccountNumber == accountNumber)
+                    .Where(r => r.Narration != null)
+                    .Where(r => r.Amount != null)
+                    .Where(r => r.Balance != null)
+                    .OrderBy(r => r.Date)
+                    .ToListAsync();
+
+                foreach (RawBankTransaction rawBankTransaction in accountEntries)
+                {
+                    if (rawBankTransaction.Balance == previousBalance + rawBankTransaction.Amount)
+                    {
+                        previousBalance = (int)rawBankTransaction.Balance;
+                        continue;
+                    }
+                    else
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid)
+                {
+                    rawBankBalancesClosed.Add(accountNumber.ToString());
+                    List<BankTransaction> bankTransactionsToAdd = accountEntries
+                        .Select(
+                            r =>
+                                new BankTransaction
+                                {
+                                    Id = r.Id,
+                                    AccountNumber = r.AccountNumber,
+                                    Date = r.Date,
+                                    Narration = r.Narration,
+                                    Amount = r.Amount,
+                                    Balance = r.Balance
+                                }
+                        )
+                        .ToList();
+                    _dbContext.BankTransactions.AddRange(bankTransactionsToAdd);
+                    _dbContext.SaveChanges();
+                }
+            }
+
+            var message = "Closed the following bank balances: " + string.Format("{0}", string.Join(", ", rawBankBalancesClosed));
+            _logger.LogInformation(message);
+            return Ok(message);
+        }
+        catch (Exception ex)
+        {
+            var errorMessage = "Could not close accounts";
+
+            _logger.LogError(errorMessage);
+            _logger.LogError(ex.Message);
+            return Conflict(errorMessage);
+        }
+    }
 
     [HttpGet("api/BankTransaction/all")]
     public async Task<ActionResult> GetBankTransactionAsync()
     {
         var bankTransactions = await _dbContext.BankTransactions.ToListAsync();
+
         _logger.LogInformation("Retrieved all of the bank transactions");
         return Ok(bankTransactions);
     }
@@ -50,6 +117,7 @@ public class TransactionController : ControllerBase
             );
             return NotFound("Id: " + id + " does not exist");
         }
+
         _logger.LogInformation("Retrieved the bank transaction: id = " + id.ToString());
         return Ok(bankTransaction);
     }
@@ -58,6 +126,7 @@ public class TransactionController : ControllerBase
     public async Task<ActionResult> GetRawBankTransactionAsync()
     {
         var rawBankTransactions = await _dbContext.RawBankTransactions.ToListAsync();
+
         _logger.LogInformation("Retrieved all of the raw bank transactions");
         return Ok(rawBankTransactions);
     }
@@ -87,6 +156,7 @@ public class TransactionController : ControllerBase
         _logger.LogInformation(
             "Added the raw bank transaction: id = " + rawBankTransaction.Id.ToString()
         );
+
         return Ok(rawBankTransaction.AccountNumber);
     }
 
@@ -100,6 +170,7 @@ public class TransactionController : ControllerBase
             _dbContext.RawBankTransactions.Any(r => r.Id == rawBankTransaction.Id);
             _dbContext.Entry(rawBankTransaction).State = EntityState.Modified;
             await _dbContext.SaveChangesAsync();
+
             _logger.LogInformation(
                 "Updated the raw bank transaction: id = " + rawBankTransaction.Id.ToString()
             );
@@ -110,7 +181,8 @@ public class TransactionController : ControllerBase
             var errorMessage =
                 "Could not update the raw bank transaction: id = "
                 + rawBankTransaction.Id.ToString();
-            _logger.LogInformation(errorMessage);
+            _logger.LogError(errorMessage);
+
             _logger.LogError(ex.Message);
             return Conflict(errorMessage);
         }
@@ -127,17 +199,18 @@ public class TransactionController : ControllerBase
                 _logger.LogInformation("Could not find the raw bank transaction: id = " + id);
                 return NotFound();
             }
+
             _dbContext.RawBankTransactions.Remove(rawBankTransaction);
             await _dbContext.SaveChangesAsync();
+
             _logger.LogInformation("Deleted the raw bank transaction: id = " + id);
             return NoContent();
         }
         catch (Exception ex)
         {
-            var errorMessage =
-                "Could not delete the raw bank transaction: id = "
-                + id.ToString();
-            _logger.LogInformation(errorMessage);
+            var errorMessage = "Could not delete the raw bank transaction: id = " + id.ToString();
+            _logger.LogError(errorMessage);
+
             _logger.LogError(ex.Message);
             return Conflict(errorMessage);
         }
